3c078ad94a9760d9b4b5f95a92180eff0e8e2255
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-30 16:36:44 +0100

    theme data

--- lib/src/utils/theme_data.dart
+++ lib/src/utils/theme_data.dart
@@ -16,13 +16,31 @@ final lightTheme = ThemeData(
   colorScheme: ColorScheme.fromSwatch().copyWith(
     secondary: Colors.white,
   ),
-  //primaryColor: Colors.white,
+  primaryColor: Colors.white,
   brightness: Brightness.light,
-  textTheme: const TextTheme(
-    subtitle1: TextStyle(color: Colors.black)
-  ),
+  textTheme: const TextTheme(subtitle1: TextStyle(color: Colors.black)),
   backgroundColor: const Color(0xFFE5E5E5),
   accentColor: Colors.black,
+  dividerColor: Colors.grey,
+  iconTheme: const IconThemeData(color: Colors.black),
+  primaryIconTheme: const IconThemeData(color: Colors.black),
+);
+
+final deepTheme = ThemeData(
+  scaffoldBackgroundColor: Color.fromARGB(255, 1, 18, 43),
+  appBarTheme: AppBarTheme(color: Colors.blueGrey),
+  colorScheme: ColorScheme.fromSwatch(
+    brightness: Brightness.dark,
+    backgroundColor: Color.fromARGB(255, 3, 59, 105),
+    accentColor: Color.fromARGB(255, 3, 59, 105),
+  ).copyWith(
+    secondary: Color.fromARGB(255, 2, 77, 189),
+  ),
+  primaryColor: Color.fromARGB(255, 2, 38, 155),
+  textTheme: const TextTheme(
+      subtitle1: TextStyle(color: Color.fromARGB(255, 12, 239, 247))),
+  backgroundColor: Color.fromARGB(255, 1, 41, 151),
+  accentColor: Color.fromARGB(255, 32, 1, 170),
   dividerColor: Colors.grey,
   iconTheme: const IconThemeData(color: Colors.black),
   primaryIconTheme: const IconThemeData(color: Colors.black),
