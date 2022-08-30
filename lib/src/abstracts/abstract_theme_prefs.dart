2a0d4c71f64a0b40e89a8cc837dd7ac177f321fe
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-30 09:52:49 +0100

    theming

---
+++ lib/src/abstracts/abstract_theme_prefs.dart
@@ -0,0 +1,7 @@
+abstract class ThemePreference{
+  /// set Data
+  void setData(String key, dynamic value);
+
+  /// read Data
+  String readData(String key);
+}
---
+++ lib/src/utils/theme_prefs.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:themeing/src/abstracts/abstract_theme_prefs.dart';
+
+class ThemePreferenceimpl extends ThemePreference{
+  late final SharedPreferences pref;
+
+  ThemePreferenceimpl(this.pref);
+
+  @override
+  String readData(String key) {
+    dynamic obj = pref.get(key);
+    return obj;
+  }
+
+  @override
+  void setData(String key, value) {
+    pref.setString(key, value);
+  }
+
+}
