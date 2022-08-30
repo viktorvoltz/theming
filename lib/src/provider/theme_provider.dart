1fcbbf6e67c079dde346212c632da84f7d29c8af
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-30 13:56:18 +0100

    state: theme provider

--- lib/src/provider/theme_provider.dart
+++ lib/src/provider/theme_provider.dart
@@ -4,37 +4,55 @@ import 'package:themeing/src/abstracts/abstract_theme_prefs.dart';
 import '../utils/theme_data.dart';
 import '../utils/theme_prefs.dart';
 
-class ThemeProvider extends ChangeNotifier{
-
+class ThemeProvider extends ChangeNotifier {
   static ThemeData? _themeData;
   ThemeData getTheme() => _themeData!;
 
+  static late String themeMode;
+  dynamic get themeModeValue => themeMode;
+
   String themeValue = "themeMode";
   late ThemePreference themePreferenceimpl;
-  ThemeProvider(pref){
+  ThemeProvider(pref) {
     themePreferenceimpl = ThemePreferenceimpl(pref);
   }
 
   /// assert the App's theme on start up.
-  assertTheme(){
+  assertTheme() {
     dynamic value = themePreferenceimpl.readData(themeValue);
-      var themeMode = value ?? 'light';
-      if (themeMode == 'light') {
-        _themeData = lightTheme;
-      } else {
-        _themeData = darkTheme;
-      }
-      notifyListeners();    
+    themeMode = value ?? 'light';
+    if (themeMode == 'light') {
+      _themeData = lightTheme;
+    } else {
+      _themeData = darkTheme;
+    }
+    notifyListeners();
+  }
+
+  setTheme(String value) {
+    switch (value) {
+      case "light":
+        setLightMode();
+        break;
+      case "dark":
+        setDarkMode();
+        break;
+      default:
+        setLightMode();
+        break;
+    }
   }
 
   void setDarkMode() async {
     _themeData = darkTheme;
+    themeMode = "dark";
     themePreferenceimpl.setData(themeValue, 'dark');
     notifyListeners();
   }
 
   void setLightMode() async {
     _themeData = lightTheme;
+    themeMode = "light";
     themePreferenceimpl.setData(themeValue, 'light');
     notifyListeners();
   }
